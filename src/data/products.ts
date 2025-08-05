8ee685eafcc42e88df7a8ab9586252875ac7ecb3
Shriharipise18 <piseshrihari@gmail.com>
2025-08-05 11:50:55 +0530

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -53,7 +53,7 @@ export const sampleProducts: Product[] = [
       phone: '+91-9345678901',
       email: 'suresh.grains@gmail.com'
     },
-    image: 'https://www.pexels.com/photo/grass-field-during-golden-hour-533982/',
+    image: 'https://plus.unsplash.com/premium_photo-1670909649532-d1d68ee475cd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2hlYXR8ZW58MHx8MHx8fDA%3D',
     description: 'High-quality wheat grains perfect for making flour and bread.',
     inStock: true,
     harvestDate: '2024-03-10'
